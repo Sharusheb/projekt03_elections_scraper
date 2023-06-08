a6209203a58525636073a24ceca7b34b196c2ecf
Sarka Bohmova <sarka.bohmova@seznam.cz>
2023-06-08 10:39:53 +0200

    final kod

--- scraper.py
+++ scraper.py
@@ -5,82 +5,114 @@ discord: ShareenBoom#6281
 """
 
 import requests
-import csv
-import sys
-from urllib.parse import urljoin
 from bs4 import BeautifulSoup
+import sys 
+import csv
 
-def scrape_results(url, output_file):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find the link for selecting a district
-    links = soup.find_all('a', href=True)
-    if not links:
-        print("Link for selecting a district not found.")
-        return
+def get_rows(url: str) -> list:                                                 #Funkce získá všechna data z adresy URL
+    print(f"Stahuji data z vložené URL: {url}")
+    tables = BeautifulSoup(requests.get(url).text, 'html.parser').find_all\
+    ("table", {"class": "table"})
+    all_rows = [row for table in tables for row in table.find_all("tr")[2:]]
+    return all_rows
 
+
+def get_code_location(all_rows: list) -> tuple:                                 #Funkce získá kód a umístění z tabulky
+    code = [row.find_all("td")[0].text for row in all_rows
+            if row.find_all("td")[0].text != "-"]
+    location = [row.find_all("td")[1].text for row in all_rows
+                if row.find_all("td")[1].text != "-"]
+    return code, location
+
+
+def tables_detail(all_rows: list) -> tuple:                                     #Funkce "scrapne" URL a získá data
+    base_url = "https://www.volby.cz/pls/ps2017nss/"
+    tab_district, tab_vote = [], []
+    links = [base_url + row.find("a", href=True)["href"] for row in all_rows
+             if row.find("a", href=True) is not None]
     for link in links:
-        district_url = link.get('href')
-        if district_url and not district_url.startswith('javascript:'):
-            district_url = urljoin(url, district_url)
-            response = requests.get(district_url)
-            soup = BeautifulSoup(response.content, 'html.parser')
-
-            # Find links for all municipalities
-            municipality_links = soup.find_all('a', href=True)
-            if not municipality_links:
-                print("Links for municipalities not found.")
-                return
-
-            with open(output_file, 'a', newline='', encoding='utf-8') as csvfile:
-                writer = csv.writer(csvfile)
-
-                for municipality_link in municipality_links:
-                    municipality_url = municipality_link['href']
-                    if not municipality_url.startswith('javascript:'):
-                        municipality_url = urljoin(district_url, municipality_url)
-                        response = requests.get(municipality_url)
-                        soup = BeautifulSoup(response.content, 'html.parser')
-
-                        municipality_name_element = soup.find('h3')
-                        if municipality_name_element:
-                            municipality_name = municipality_name_element.text.strip()
-                        else:
-                            municipality_name = "N/A"  # Default value if element not found
-
-                        voters = soup.find_all('td', class_='cislo')[:3]
-                        voters_data = [int(vol.text.replace('\xa0', '').replace(',', '')) for vol in voters]
-
-                        writer.writerow([municipality_link.text, municipality_name] + voters_data)
-
-                        # Find additional data and write it to the CSV file
-                        results_table = soup.find('table', class_='table')
-                        if results_table:
-                            results_rows = results_table.find_all('tr')[1:]  # Skip the header
-                            for row in results_rows:
-                                result_data = [cell.text.strip() for cell in row.find_all('td')]
-                                writer.writerow(result_data)
-
-                        # Find political parties and write them to the CSV file
-                        parties_table = soup.find('table', class_='table')
-                        if parties_table:
-                            parties_rows = parties_table.find_all('tr')[1:]  # Skip the header
-                            for row in parties_rows:
-                                party_data = [cell.text.strip() for cell in row.find_all('td')]
-                                writer.writerow(party_data)
-
-            print(f"Results have been saved to the file {output_file}.")
+        soup = BeautifulSoup(requests.get(link).text, 'html.parser')
+        tab_district.append(soup.find_all("table", {"class": "table"})[0])
+        tab_vote.extend(soup.find_all("table", {"class": "table"})[1:3])
+    return tab_district, tab_vote
 
 
-if __name__ == '__main__':
+def get_votes(tab_district: list) -> tuple:                                     #Funkce získá počty registrovaných voličů, počet obálek a počet platných hlasů
+    register = [tab.find("td",{"class": "cislo", "data-rel": "L1",
+                         "headers": "sa2"}).text.replace("\xa0","") 
+                         for tab in tab_district]
+    envelope = [tab.find("td", {"class": "cislo", "data-rel": "L1",
+                         "headers": "sa3"}).text.replace("\xa0","") 
+                         for tab in tab_district]
+    valid = [tab.find("td", {"class": "cislo", "data-rel": "L1",
+                         "headers": "sa6"}).text.replace("\xa0","") 
+                         for tab in tab_district]
+    return register, envelope, valid
+
+
+def get_head(tab_vote: list) -> list:                                           #Funkce se dostane do "hlavičky" tabulky
+    head = [tr.find("td", {"class": "overflow_name"}).text 
+            for tr in tab_vote[0].find_all("tr")[2:len(tab_vote[0])]]
+    head.extend([tr.find("td", {"class": "overflow_name"}).text 
+                 for tr in tab_vote[1].find_all("tr")[2:len(tab_vote[1])]])
+    return head
+
+
+def results(tab_vote: list) -> list:                                            #Funkce vrátí data o počtu platných hlasů pro jednotlivé strany
+    result_1 = []
+    result_2 = []
+    for rows in tab_vote:
+        value_1 = rows.find_all("td", {"class": "cislo", 
+                                       "headers": "t1sa2 t1sb3"})
+        value_2 = rows.find_all("td", {"class": "cislo", 
+                                       "headers": "t2sa2 t2sb3"})
+        values_1 = [v.text.replace("\xa0", "") for v in value_1]
+        values_2 = [v.text.replace("\xa0", "") for v in value_2]
+        if values_1:
+            result_1.append(values_1)
+        if values_2:
+            result_2.append(values_2)
+    final_list = list(zip(*result_1)) + list(zip(*result_2))
+    return final_list
+
+
+def create_dict(head: list, final_list: list) -> dict:                          #Funkce vytvoří slovník. Název strany = klič. Počet hlasů = hodnota.
+    dict_vote = {}
+    for i, party in enumerate(head):
+        dict_vote[party] = final_list[i]
+    return dict_vote
+
+
+def scrape_url(url: str) -> dict:                                               #Funkce "srapne" URL a vráti všechna data pro vyýstup 
+    all_rows = get_rows(url)
+    tab_district, tab_vote = tables_detail(all_rows)
+    code, location = get_code_location(all_rows)
+    register, envelope, valid = get_votes(tab_district)
+    dict_vote = create_dict(get_head(tab_vote), results(tab_vote))
+    data_all = {"code":code, "location": location, "registered": register,
+                "envelopes": envelope, "valid": valid,}
+    data_all.update(dict_vote)
+    return data_all
+
+
+def save_to_csv(file_name: str, data_all: dict):                                #Funkce uloží data do CSV souboru
+    with open(file_name, mode='w', newline='', encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(data_all.keys())
+        writer.writerows(zip(*data_all.values()))
+
+
+def main():                                                                     #Funkce spustí program
     if len(sys.argv) != 3:
-        print("Incorrect usage. Please use: python your_script.py url output.csv")
-        sys.exit(1)
+        print("Musíte zadat 3 argumenty do terminálu\
+(název hlavního soubouru, zkopírované URL stránky, název výsledného souboru v CSV)")
+    else:
+        url = sys.argv[1]
+        csv = sys.argv[2]
+        save_to_csv(csv, scrape_url(url))
+        print(f"Váš výstup je uložen v {csv}")
+    
 
-    url = sys.argv[1]
-    output_file = sys.argv[2]
-
-    scrape_results(url, output_file)
-
-                
+if __name__ == "__main__":
+    main()  
